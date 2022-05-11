2289f08fb40c1a64bc8757dd8df144534abc530a
mays2pearl <maysa.binho.perola@hotmail.com>
2022-05-11 15:24:40 -0300

    criando um formulario

--- projetoreact/src/Maysa.js
+++ projetoreact/src/Maysa.js
@@ -1,42 +1,53 @@
-import axios from "axios";
+import axios from "axios"; /*Para se comunicar com a api, utilizaremos uma lib especializada em processos http, a lib no caso é a axios. */
 import { useEffect, useState } from "react";
 
 
 function Maysa(){
 
-    const [usuarios, setUsuarios] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);/*useState Como teremos uma variavel que será dinamica, devemos utilizar esse hook, pois ele é responsável por cuidar da atualização em tempo real dessa variavel.*/
 
-
-  useEffect(() => {
-        axios.get('https://iot.14mob.com/lista.json').then( response => {
-            setUsuarios(response.data);
+  useEffect(() => { /*useEffect Esse hook será executado toda vez que a tela for carregada, ou seja, utilizaremos ele para carregar nossa listagem inicial de usuarios. */
+        axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
+            setUsuarios(response.data.users);
             console.log(response);
         })
     },[])
 
 
   return (
-    <table className='minhaTabela'>
-        <thead>
-            <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            </tr>
-        
-        </thead>  
-        <tbody> 
-            { usuarios.map( usuario => {
-                return (
-                    <tr key={usuario.id}>
-                        <td>{usuario.id}</td>
-                        <td>{usuario.nome}</td>
-                        <td>{usuario.descricao}</td>
-                    </tr>
-                    )
-            } ) }
-        </tbody>
-      </table>
+      <div className="formulario">
+        <form>
+        <label>Nome</label>
+        <input name="name" value="" />
+        <label>Email</label>
+        <input name="email" value="" />
+        <label>senha</label>
+        <input name="password" value="" />
+        <button type="submit">Enviar</button>  
+        </form>
+        <table className='minhaTabela'>
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                </tr>
+            
+            </thead>  
+            <tbody> 
+                { usuarios.map( usuario => {
+                    return (
+                        <tr key={usuario.id}>
+                            <td>{usuario.id}</td>
+                            <td>{usuario.name}</td>
+                            <td>{usuario.email}</td>
+                        </tr>
+                        )
+                } ) }
+            </tbody>
+        </table>
+    </div>
+      
       
   );
 }
